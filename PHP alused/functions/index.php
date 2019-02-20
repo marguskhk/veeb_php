cbf8f53060dcdf5312ca3cf54d413738b3dc9524
marguskhk <margus.noppel@khk.ee>
2019-02-20 12:36:59 +0200

    funktsioon  echoga

---
+++ PHP alused/functions/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: margus.noppel
+ * Date: 20.02.2019
+ * Time: 12:13
+ */
+function paarsuseKontroll($arv) {
+    if ($arv % 2 == 0){
+    echo $arv.' on paaris <br> ';
+    } else {
+        echo $arv.'on paaritu <br>';
+    }
+}
+//kutsume funktsioon tööle
+for($arv=0; $arv <=10: $arv++) {
+        paarsuseKontroll($arv);
+}

1f11b24832982bfa9a923a089a6b40155845ae75
marguskhk <margus.noppel@khk.ee>
2019-02-20 15:07:23 +0200

    array + loodud lihtne massiiv ja kontrollitud test sisu

---
+++ PHP alused/array/index.php
@@ -0,0 +1,12 @@
+<?php
+    $peppaPig = array{
+        'Peppa',
+        'naine',
+        4,
+        1.04
+};
+    var_dump($peppaPig);
+
+    echo '<pre>';
+    print_r($peppaPig);
+    echo '<pre>';

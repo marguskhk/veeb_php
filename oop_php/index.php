c9a7485ae03840e4e66f0f9eae8bbfddf0fb82ae
marguskhk <margus.noppel@khk.ee>
2019-03-27 13:52:38 +0200

    OOP_PHP menu, failide ümberpaigitused kaustade vahel

--- oop_php/index.php
+++ oop_php/index.php
@@ -2,10 +2,10 @@
 require_once 'conf.php';
 
 // main page by html templates
-$main = new Template('main');
-$meta = new Template('meta');
-$style = new Template('style');
-$js = new Template('js');
+$main = new Template('app.main');
+$meta = new Template('app.meta');
+$style = new Template('app.style');
+$js = new Template('app.js');
 
 // add meta, style and js templates content to main template
 $main->set('meta', $meta->parse());
@@ -17,22 +17,22 @@ $main->set('lang', $http->get('lang_id'));
 $main->set('title', 'App Example Title');
 
 
-$mainContent = new Template('main_content');
+$mainContent = new Template('menu.main_content');
 
 // create dish types selection
 $sql = 'SELECT * FROM dish_types';
 $dishTypes = $db->getData($sql);
 foreach ($dishTypes as $dishTypeData){
-    $dishType = new Template('type');
+    $dishType = new Template('menu.type');
 
-    $dishTypeName = new Template('type_name');
+    $dishTypeName = new Template('menu.type_name');
     $dishTypeName->set('type_name', $dishTypeData['type_name']);
     $dishTypeName->set('type_icon', $dishTypeData['type_icon']);
     $dishType->set('type_name', $dishTypeName->parse());
 
-    $dishData = new Template('type_data');
+    $dishData = new Template('menu.type_data');
     $dishData->set('type_name', $dishTypeData['type_name']);
-    $dish = new Template('dish');
+    $dish = new Template('menu.dish');
     $sql = 'SELECT * FROM dishes WHERE type_id='.fixDb($dishTypeData['type_id']);
     $dishes = $db->getData($sql);
 
